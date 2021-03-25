3d3fc389d6fb72c4b0a8c6892d45a4dee14a16d2
pratyush19919 <52246459+pratyush19919@users.noreply.github.com>
2021-03-26 01:17:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,78 +1,99 @@
-from flask import Flask, Response, render_template
+# from flask import Flask,render_template
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return render_template("index.html")
+
+# if (__name__)==("__main__"):
+#     app.run(debug=True)
+from flask import Flask, render_template,redirect,request,url_for
+from PIL import Image
+
 import cv2
 import numpy as np
 import os
-import datetime
 
-recognizer=cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trained_data\\trainer.yml')
-face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default_copy.xml")
-font=cv2.FONT_HERSHEY_SIMPLEX
-
-id=0
-cap=cv2.VideoCapture(0,cv2.CAP_DSHOW)
-cap.set(3,640)
-cap.set(4,480)
-
-winW = 0.1*cap.get(3)
-winH = 0.1*cap.get(4)
-
-names=["None","Sai","Bhavana","Bhaskar","Sujatha","1"]
-
-
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 app = Flask(__name__)
-
-
-def get_names(cap):
-    people = set()
-    while True:
-        ret,img=cap.read()
-        gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-        faces=face_cascade.detectMultiScale(gray,1.2,5,minSize=(int(winW),int(winH)))
-        for x,y,w,h in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),5)
-            id,confidence=recognizer.predict(gray[y:y+h,x:x+w])
-            if confidence<100:
-                id=names[id]
-                confidence="{0}%".format(round(100-confidence))
-            else:
-                id="unknown"
-                confidence="{0}%".format(round(100-confidence))
-            
-            if id == "Sai":
-                people.add(id)
-            if id == "Bhavana":        
-                people.add(id)
-            if id == "Bhaskar":         
-                people.add(id)
-            if id == "Sujatha":    
-                people.add(id)
-            
-            cv2.putText(img,str(id),(x+5,y-5),font,1,(255,255,0),3)
-            cv2.putText(img,str(confidence),(x+5,y+h-5),font,1,(255,255,0),2)
-        
-        cv2.imshow("vid",img)
-        cv2.waitKey(10)
-        lpeople = list(people)
-        if len(lpeople) > 0:
-            break
-        k = cv2.waitKey(10) & 0xff 
-        if k == 27:
-            break
-    
-    return lpeople
-    
-    
-
 
 @app.route('/')
 def index():
-    global cap
-    people = get_names(cap)
-    #l_people = [p for p in people]
-    return render_template("index.html", people=people, len=len(people))
+  return render_template('index.html')
 
+@app.route('/send',methods=["GET","POST"])
+def cap_data():
+    
+    
+    # if request.method=="POST":
+    #     id_student = request.form.get("id_student")
+    #     name = request.form.get("name")
+        
+    #     dirName="Images/"+name
+        
+    #     os.mkdir(dirName)
+
+    
+    
+    # cap = cv2.VideoCapture(0)
+    # count = 0
+    # while True:
+
+    #     ret, frame = cap.read()
+    #     if  frame is not None:
+    #         count += 1
+    #         faces = face_classifier.detectMultiScale(frame, 1.3, 5)
+            
+        
+            
+        
+            
+    #         # Crop all faces found
+    #     for (x,y,w,h) in faces:
+    #         x=x-10
+    #         y=y-10
+    #         cropped_face = frame[y:y+h+50, x:x+w+50]
+
+    #         face = cv2.resize(cropped_face, (400, 400))
+    #         #face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+    #         # Save file in specified directory with unique name
+    #         file_name_path = dirName + "/"+ str(count) + '.jpg'
+    #         cv2.imwrite(file_name_path, face)
+
+    #         # Put count on images and display live count
+    #         cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+    #         cv2.imshow('Face Cropper', face)
+    #     else:
+    #         print("Face not found")
+    #         pass
+                 
+
+    #     if cv2.waitKey(1) == 13 or count == 200: #13 is the Enter Key
+    #         break
+
+        
+
+
+    # cap.release()
+    # cv2.destroyAllWindows()      
+    # print("Collecting Samples Complete")
+        
+    return render_template('train.html')
+
+@app.route('/train',methods=["GET","POST"])
+def train_face():
+    faces_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default_copy.xml")
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    
+
+
+    
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+  app.run(debug=True)
+
+
+
